7173b5b7e2424822a3d7020c39a25d50b985cf61
Victor Hugo Guardiola <victorhugo.pessoal.guardiola@gmail.com>
2024-12-26 11:15:12 +0100

    iteration 4 |Filter the Products

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
-import jsonData from "../data.json"
+import jsonData from "../data.json";
 import SearchBar from "./SearchBar";
 import ProductTable from "./ProductTable";
 
 function ProductsPage() {
 
-    const [products, setProducts] = useState(jsonData);
+  const [searchValue, setSearchValue] = useState("");
 
-    return(
-        <div>
-            <h1>IronStore</h1>
-            <SearchBar />
-            <ProductTable products={products}/>
-        </div>
-    )
+  
+  const filteredProducts = jsonData.filter((eachProduct) => 
+    eachProduct.name.toLowerCase().includes(searchValue.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h1>IronStore</h1>
+      <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
+      <ProductTable products={filteredProducts} />
+    </div>
+  );
 }
 
 export default ProductsPage;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,17 @@
-function SearchBar() {
+function SearchBar(props) {
+
+    // console.log(props);
+    const { searchValue, setSearchValue } = props;
+
+    const handleSearch = (event) => {
+        setSearchValue(event.target.value);
+    }
+
   return (
     <div>
       <label>
         <p>Search</p>
-        <input type="text" />
+        <input type="text" value={searchValue} onChange={handleSearch} />
       </label>
       <div>
         <label>
